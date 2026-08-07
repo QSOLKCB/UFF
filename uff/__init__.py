b871f7bb195245cb60cd4c017a906b96e4f2b11e
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2026-08-07 09:53:28 +0930

    Bump UFF package version to 5.0.0

--- uff/__init__.py
+++ uff/__init__.py
@@ -1,4 +1,4 @@
-"""QSOL UFF: transparent galaxy and compact-object model laboratory."""
+"""QSOL UFF: reproducible astrophysics and falsification laboratory."""
 
 from .compact import (
     compact_object_report,
@@ -11,7 +11,7 @@ from .fitting import FitResult, fit_model, fit_models
 from .models import ModelOptions, available_models, build_model
 from .sampling import PosteriorResult, sample_posterior
 
-__version__ = "4.0.0"
+__version__ = "5.0.0"
 
 __all__ = [
     "FitResult",
